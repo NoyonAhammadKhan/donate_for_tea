c31e016e339c4d9f55a247d84b58434030547f64
NoyonAhammadKhan <noyonahammadkhan6464@gmail.com>
2023-01-22 02:19:27 -0500

    testing is working

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,8 +6,59 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+async function getBalances(address){
+  const balanceBigInt = await hre.ethers.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+}
+
+async function consoleBalances(addresses){
+  let counter=0;
+  for(const address of addresses){
+    console.log(`Address ${counter} balance`,await getBalances(address));
+    counter++;
+  }
+}
+
+async function countMemos(memos){
+  for(const memo of memos){
+    const timestamp = memo.timestamp;
+    const name = memo.name;
+    const from = memo.from;
+    const message = memo.message;
+    console.log(`At ${timestamp},name ${name}, address ${from} ,message ${message}`);
+
+
+  }
+}
+
 async function main() {
-  
+  const [owner, from1,from2,from3]=await hre.ethers.getSigners();
+  const chai = await hre.ethers.getContractFactory("chai");
+  const contract = await chai.deploy();
+
+  await contract.deployed();
+  console.log("Addresses of contract",contract.address);
+
+  const addresses = [owner.address,
+                    from1.address,
+                    from2.address,
+                    from3.address];
+
+  console.log("before buying chai");
+  await consoleBalances(addresses);
+
+  const amount ={value:hre.ethers.utils.parseEther("1")};
+
+  await contract.connect(from1).buyChai("from1","very nice chai",amount)
+  await contract.connect(from2).buyChai("from2","very nice course",amount)
+  await contract.connect(from3).buyChai("from3","very nice information",amount)
+
+  console.log("after buying chai");
+  await consoleBalances(addresses);
+
+  const memos = await contract.getMemos();
+  countMemos(memos);
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
